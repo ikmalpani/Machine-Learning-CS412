d7eaaedbc5c889a4c70cb9bca1e8a23170cb240d
ikmalpani <malpanikeshav95@gmail.com>
2016-11-25 17:05:58 -0600

    Update 1-data.lua

--- torch/1-data.lua
+++ torch/1-data.lua
@@ -1,1 +1,28 @@
+print("==> Loading data...")
+
+DATASET_SIZE = 1
+
+--------------------------------------------------------------------------------
+-- Load data from csv file
+
+function load_data(path)
+	local file = assert(io.open(path, "r"))
+	local data, label = {}, {}
+	local line
+
+	for i = 0, 144 do
+		line = file:read()
+		for j = 1, 143 do
+			data = line[i][j]
+		label = line[i][144]
+	end
+
+	return data, label
+end
+
+
+--------------------------------------------------------------------------------
+-- Test
 --
+--data, label = load_data("Time_struct_subject0_trial0_aditi2.csv")
+--print(data)
