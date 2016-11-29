24348b39c97ec2a80667255a8addc887f768e7aa
skalkur <sharathkalkur1993@gmail.com>
2016-11-29 16:57:59 -0600

    Update 2-model.lua

--- torch/2-model.lua
+++ torch/2-model.lua
@@ -1,16 +1,16 @@
-function build_nn(n_inputs, n_layers, n_hidden,n_output, opt)
+function build_nn(n_inputs, opt,n_output)
 
 	local network= nn.Sequential()
 	local p=opt.dropout_prob
 	-- Activation fn could be Tanh as well, will change according to the requirements
-	network.add:(nn.Linear(n_inputs, n_hidden))
+	network.add:(nn.Linear(n_inputs, opt.n_hidden))
 	network.add:(nn.Dropout(p))
 	network.add:(nn.Sigmoid())
 
 	-- Since it is a simple set of features, multi-layer is not required, will change as per the performance of the NN
 	[[
 
-	for i=1,n_layers-1 do
+	for i=1,opt.n_layers-1 do
 		network.add:(nn.Linear(n_hidden, n_hidden))
 		network.add:(nn.Dropout(p))
 		network.add:(nn.Sigmoid())
@@ -18,7 +18,7 @@ function build_nn(n_inputs, n_layers, n_hidden,n_output, opt)
 
 	]]
 
-	network.add:(nn.Linear(n_hidden, n_output))
+	network.add:(nn.Linear(opt.n_hidden, n_output))
 	network.add:(nn.Sigmoid()) -- Ideal for binary classification	
 	--network.add:(nn.LogSoftMax()) -- Choose this if classes are more than 2
 
