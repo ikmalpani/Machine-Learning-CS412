19068b3209668b2d9dda36de319c4fd3ba2bf000
skalkur <sharathkalkur1993@gmail.com>
2016-12-07 19:41:56 -0600

    Final model.lua

--- torch/2-model.lua
+++ torch/2-model.lua
@@ -1,32 +1,27 @@
-function build_nn(n_inputs, opt, n_output)
+function build_nn(n_inputs,opt,n_output)
 
+	--require "nn"
+	local n_hidden=opt.n_hidden
 	local network= nn.Sequential()
-	local p=opt.dropout_prob
+	
 	-- Activation fn could be Tanh as well, will change according to the requirements
-	network:add(nn.Linear(n_inputs, opt.n_hidden))
-	network:add(nn.Dropout(p))
+
+	network:add(nn.Linear(n_inputs, n_hidden+10))
 	network:add(nn.Sigmoid())
 
-	-- Since it is a simple set of features, multi-layer is not required, will change as per the performance of the NN
-	--[[
+	network:add(nn.Linear(n_hidden+10, n_hidden))
+	network:add(nn.Sigmoid())
 
-	for i=1,opt.n_layers-1 do
-		network.add:(nn.Linear(n_hidden, n_hidden))
-		network.add:(nn.Dropout(p))
-		network.add:(nn.Sigmoid())
-	end
 
-	--]]
-
-	network:add(nn.Linear(opt.n_hidden, n_output))
-	--network:add(nn.Sigmoid()) -- Ideal for binary classification	
+	network:add(nn.Linear(n_hidden, n_output))
+	--network.add:(nn.Sigmoid()) -- Ideal for binary classification	
 	network:add(nn.LogSoftMax()) -- Choose this if classes are more than 2
 
 	--local criterion=nn.MSECriterion() -- If using this, output must be binary or a 1-hot encoded Tensor, use with Sigmoid
 	
-	local criterion=nn.ClassNLLCriterion() --Use this if using SoftMax at output
+	criterion=nn.ClassNLLCriterion() --Use this if using SoftMax at output
 
-
+	print(network)
 	return network, criterion
 
 end
