7c14a7a15ff1114226b80ab9140a05d1c01c590a
skalkur <sharathkalkur1993@gmail.com>
2016-12-07 19:41:27 -0600

    Final data.lua

--- torch/1-data.lua
+++ torch/1-data.lua
@@ -1,51 +1,27 @@
--------------------------------------------------------------------------------
--- String splitter
--- Reference: http://stackoverflow.com/questions/1426954/split-string-in-lua
---
-function mysplit(inputstr, sep)
-  if sep == nil then
-    sep = "%s"
-  end
-  local t={} ; i=0
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-    i = i + 1
-    t[i] = str
-  end
-  return t
+-- References: http://stackoverflow.com/questions/22935906/string-format-and-gsub-in-lua 
+-- http://mdtux89.github.io/2015/12/11/torch-tutorial.html
+
+-- Function to split the data at commas
+function string:splitAtCommas()
+  local sep, values = ",", {}
+  local pattern = string.format("([^%s]+)", sep)
+  self:gsub(pattern, function(c) values[#values+1] = c end)
+  return values
 end
 
-
--------------------------------------------------------------------------------
--- Load data
---
-function load_data(file)
-  local data, label = {}, {}
-  local curr_size = 0
-
-  local f = assert(io.open(file, "r"))
-  local line
-
-  -- Read all the way to the last line.
-  while true do
-    line = f:read()
-
-    if line ~= nil then
-      curr_size = curr_size + 1
-      data[curr_size] = {}
-      local values = mysplit(line, ",")
-
-      -- Data
-      for i = 1, #values-1 do
-          data[curr_size][i] = tonumber(values[i])
-      end
-
-      -- Label
-      label[curr_size] = values[#values]
-
-    else break end
-  
+-- Load the features and labels into Tensor
+function loadData(dataFile)
+  local dataset = {}
+local i=0
+  for line in io.lines(dataFile) do
+    local values = line:splitAtCommas()
+    local y = torch.Tensor(1)
+    y[1] = values[#values]
+    values[#values] = nil
+    local x = torch.Tensor(values)
+    dataset[i] = {x, y}
+    i = i + 1
   end
-  f:close()
-
-	return data,label
+  function dataset:size() return (i - 1) end 
+  return dataset
 end
